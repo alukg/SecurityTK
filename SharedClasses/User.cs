4611c8066bcddda86b6cb53ff50cf8f475263598
KerenHerman <kerenherman1@gmail.com>
2016-04-06 12:43:40 +0300

    user

--- SharedClasses/User.cs
+++ SharedClasses/User.cs
@@ -6,42 +6,7 @@ using System.Threading.Tasks;
 
 namespace SharedClasses
 {
-    public class User
+    class User
     {
-        private string username;
-        private string password;
-        //private int line;
-        
-        public User(string username)
-        {
-
-            this.username = username;
-        }
-
-
-        public User(string username, string password)
-        {
-            this.username = username;
-            this.password = password;
-        }
-
-      /*  public int getLine()
-        {
-            return line;
-        }*/
-
-        public string getUsername()
-        {
-            return username;
-        }
-        public string getPassword()
-        {
-            return password;
-        }
-
-        public void setPassword(string pass)
-        {
-            password = pass;
-        }
     }
 }
