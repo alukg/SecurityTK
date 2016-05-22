10e071cad4f6bd911c5fa65c51b2d6f1ca4aa8b0
KerenHerman <kerenherman1@gmail.com>
2016-05-22 22:54:51 +0300

    updates

--- PL_GUI/DataLeakageWindow.xaml.cs
+++ PL_GUI/DataLeakageWindow.xaml.cs
@@ -21,7 +21,6 @@ namespace PL_GUI
     {
         IBL theBL;
         string urlAdress;
-        //    List<DataFile> files;
         ObservableCollection<DataFile> files;
 
         public DataLeakageWindow(IBL theBL)
@@ -29,9 +28,6 @@ namespace PL_GUI
             this.theBL = theBL;
             InitializeComponent();
             files = new ObservableCollection<DataFile>();
-
-            //CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(files);
-
         }
 
         public void Run()
@@ -41,46 +37,29 @@ namespace PL_GUI
 
         private void Open_File_Left_Button_Click(object sender, RoutedEventArgs e)
         {
-            //Files_List.ItemsSource = files;
-            //OpenFileDialog openFileDialog = new OpenFileDialog()
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             DialogResult result = fbd.ShowDialog();
-            //fbd.SelectedPath = sourceFol
             urlAdress = fbd.SelectedPath;
-            //urlAdress = File.ReadAllText(fbd.SelectedPath);
-
-            // if (fbd.ShowDialog() == DialogResult.ok)
-            // else urlAdress = null;
-           /* if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
-            {
-                urlAdress = File.ReadAllText(fbd.SelectedPath);
-                string[] files1 = Directory.GetFiles(fbd.SelectedPath);
-
-                //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
-
-            }*/
             DataLeakageTool dlt = new DataLeakageTool();
             SortedDictionary<double, FileInfo> dictionary = dlt.checkSensitivity(urlAdress);
             foreach (var item in dictionary.Reverse())
             {
+                double temp;
+                if (item.Key <= 0) temp = 0;
+                else temp = item.Key;
                 DataFile currVar = new DataFile()
                 {
                     fi = item.Value,
                     name = item.Value.Name,
-                    score = item.Key,
+                    score = temp,
                     text = "",
                     url = urlAdress
 
                 };
                 files.Add(currVar); 
-                //Sensitivity_Text.Text = Sensitivity_Text.Text + item.Value.Name + "," + item.Key;
             };
             dlt = null;
-               this.Files_List.ItemsSource = files;
-           // LogBox.ItemsSource = files;
-
-
-
+            this.Files_List.ItemsSource = files;
         }
 
         private void Back_To_Main_Menu_Left_Button_Click(object sender, RoutedEventArgs e)
@@ -90,66 +69,13 @@ namespace PL_GUI
             mm.Close();
         }
 
-        private void LoadText_Left_Click_Button(object sender, RoutedEventArgs e)
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
         {
             String theFile;
-            //DataGridRow row = sender as DataGridRow;
-            // List<String> theList = new List<String>();
-            // DataFile currDF = (DataFile)LogBox.SelectedItem;
-            DataFile currDF =  (DataFile)Files_List.SelectedItem;
-            //DataFile currDF = (DataFile)row.DataContext;
-            // System.Windows.MessageBox.Show(currDF.url);
-            //String theFile = System.IO.File.ReadAllText(@currDF.url + "\\" + currDF.name);
-            //FileInfo fl = new FileInfo(currDF.url + "\\" + currDF.name);
-            //StreamReader reader = new StreamReader(@currDF.url + "\\" + currDF.name);
-            //if (currDF.url + "\\" + currDF.name
-            StreamReader reader = currDF.fi.OpenText();
-             using (reader)
-             {
-                theFile = reader.ReadToEnd();
-                //theList.Add(reader.ReadLine());
-            }
-            // Text_Block.ItemsSource = theList; //לפתוח URL של קובץ נתון
-            //DataContext? מקבל ליסט
+            DataFile currDF = (DataFile)Files_List.SelectedItem;
+            theFile = System.IO.File.ReadAllText(@currDF.url + "\\" + currDF.name);
             Text_TextBlock.Text = theFile;
         }
-
-        /*        private void Files_List_Click(object sender, MouseButtonEventArgs e)
-                {
-                        //< EventSetter Event = "PreviewMouseLeftButtonDown"  Handler = "Files_List_Click" ></ EventSetter >
-                    var item = sender as System.Windows.Controls.ListViewItem;
-                    if(item != null && item.IsSelected)
-                    {
-                        String theFile;
-                        DataFile currDF = (DataFile)Files_List.SelectedItem;
-                        StreamReader reader = currDF.fi.OpenText();
-                        using (reader)
-                        {
-                            theFile = reader.ReadToEnd();
-
-                        }
-                        Text_TextBlock.Text = theFile;
-                    }
-                }*/
-
-                        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
-                        {
-            LoadText_Left_Click_Button(sender, e);
-            /*
-                         String theFile;
-                    System.Windows.Controls.ListViewItem row = sender as System.Windows.Controls.ListViewItem;
-                    //DataFile currDF = (DataFile)Files_List.SelectedItem;
-                    DataFile currDF = (DataFile) row.DataContext;
-                    theFile = System.IO.File.ReadAllText(@currDF.url + "\\" + currDF.name);
-                    /*StreamReader reader = currDF.fi.OpenText();
-                            using (reader)
-                            {
-                                theFile = reader.ReadToEnd();
-                                //theList.Add(reader.ReadLine());
-                            }
-
-                             Text_TextBlock.Text = theFile;*/
-                        }
     }
 
     public class DataFile
